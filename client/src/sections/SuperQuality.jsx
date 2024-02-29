b94e3978b180a83788fc87588c8b92dc387d85b9
BALU4769 <89141485+BALU4769@users.noreply.github.com>
2024-02-29 08:46:27 +0530

    hii

--- client/src/sections/SuperQuality.jsx
+++ client/src/sections/SuperQuality.jsx
@@ -1,27 +1,68 @@
-import { shoe8 } from "../assets/images"
-import Button from "../components/Button"
-
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+import aboutsimg1 from "../sections/images/aboutsimg1.jpg";
+import "../sections/SuperQuality.css";
 const SuperQuality = () => {
   return (
-    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
-      <div className="flex flex-1 flex-col">
-        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-          We Provide You <span className="text-coral-red">Super</span> <span className="text-coral-red">Quality</span> Services
-        </h2>
+    <>
+      <section
+        id="about-us"
+        className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+      >
+        <div className="flex flex-1 flex-col">
+          <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+            We Provide You <span className="text-coral-red">Super</span>{" "}
+            <span className="text-coral-red">Quality</span> Services
+          </h2>
 
-        <p className="mt-4 lg:max-w-lg info-text">Effortless Freshness, Tailored to You – Where Clean and Simple Unite, Every Wash, Every Fold.".</p>
-        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+          <p className="mt-4 lg:max-w-lg info-text">
+            Effortless Freshness, Tailored to You – Where Clean and Simple
+            Unite, Every Wash, Every Fold.".
+          </p>
+          <p className="mt-6 lg:max-w-lg info-text">
+            Our dedication to detail and excellence ensures your satisfaction
+          </p>
 
-        {/* <div className="mt-11">
+          {/* <div className="mt-11">
           <Button label="View details" />
         </div> */}
-      </div>
+        </div>
 
-      <div className="flex-1 flex justify-center items-center">
-        <img src="https://static.wixstatic.com/media/433ab0_8aded375e81e42c49e59ee7a5de3cedd~mv2.png/v1/crop/x_0,y_17,w_685,h_510/fill/w_569,h_425,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b9df27b01a8a843c7a645ab7cf122cb3.png" alt="shoe8" width={570} height={522} className="object-contain" />
-      </div>
-    </section>
-  )
-}
+        <div className="flex-1 flex justify-center items-center">
+          <img
+            src="https://static.wixstatic.com/media/433ab0_8aded375e81e42c49e59ee7a5de3cedd~mv2.png/v1/crop/x_0,y_17,w_685,h_510/fill/w_569,h_425,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/b9df27b01a8a843c7a645ab7cf122cb3.png"
+            alt="shoe8"
+            width={570}
+            height={522}
+            className="object-contain"
+          />
+        </div>
+      </section>
+
+      <section className="section-down">
+        <img src={aboutsimg1} alt="" />
+        <div className="right-textone">
+          <h1 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+            Clean And Hygiene
+          </h1>
+          <h2
+            className="font-palanquin text-2xl capitalize font-bold lg:max-w-lg"
+            color="text-coral-red"
+          >
+            Our team
+          </h2>
+          <p className="mt-6 lg:max-w-lg info-text">
+            Purely Pristine Laundry is a modern laundromat,Our company is here
+            to help our customers get their laundry done efficiently in a safe
+            and clean environment.
+            <br /> <br />
+            Our superior levels of customer satisfaction is made possible by our
+            laundry professionals.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default SuperQuality;
