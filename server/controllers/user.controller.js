66228fb38f4b22ccc7293741e798b4fe7943e989
prathyusha <142219850+ushaprathyusha@users.noreply.github.com>
2024-04-23 11:58:00 +0530

    hi

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,28 +1,18 @@
 const User = require("../models/user.model");
 exports.registerUser = async (req, res) => {
-  const {
-    name,
-    storeId,
-    userType,
-    email,
-    password,
-    confirmPassword,
-    area,
-    phoneNumber,
-  } = req.body;
+  const { fullName, storeId, userType, email, password, confirmPassword, area } = req.body;
+
 
   try {
     // Check if user with the same storeId already exists
     const existingUser = await User.findOne({ storeId });
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with the same Store ID already exists" });
+      return res.status(400).json({ message: "User with the same Store ID already exists" });
     }
 
     // Create a new user instance
     const newUser = new User({
-      name,
+      fullName,
       storeId,
       area,
       userType,
@@ -67,6 +57,8 @@ exports.registerUser = async (req, res) => {
 //   }
 // };
 
+
+
 exports.loginUser = async (req, res) => {
   const { storeId, password } = req.body;
 
@@ -93,6 +85,7 @@ exports.loginUser = async (req, res) => {
   }
 };
 
+
 exports.loginStaff = async (req, res) => {
   const { storeId, password } = req.body;
 
@@ -111,6 +104,7 @@ exports.loginStaff = async (req, res) => {
     });
   }
 };
+
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -168,34 +162,31 @@ exports.getAll = async (req, res) => {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({
-      message:
-        "An error occurred while fetching users. Please try again later.",
-    });
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'An error occurred while fetching users. Please try again later.' });
   }
 }
 
 exports.getNextStoreId = async (req, res) => {
   try {
     // Get the last store ID from the database
-    const lastStore = await User.findOne({}, {}, { sort: { storeId: -1 } });
+    const lastStore = await User.findOne({}, {}, { sort: { 'storeId': -1 } });
 
     let nextStoreId;
     if (lastStore) {
       // Increment the last store ID
       const lastIdNumber = parseInt(lastStore.storeId.substring(4));
       const nextIdNumber = lastIdNumber + 1;
-      nextStoreId = "STID" + nextIdNumber.toString().padStart(3, "0");
+      nextStoreId = 'STID' + nextIdNumber.toString().padStart(3, '0');
     } else {
       // If no store ID exists, start with STID001
-      nextStoreId = "STID001";
+      nextStoreId = 'STID001';
     }
 
     res.json({ storeId: nextStoreId });
   } catch (err) {
-    console.error("Error getting next store ID:", err);
-    res.status(500).json({ error: "Internal server error" });
+    console.error('Error getting next store ID:', err);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
@@ -205,6 +196,7 @@ exports.getAreaByStoreId = async (req, res) => {
 
   try {
     const user = await User.findOne({ storeId });
+    
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -214,6 +206,7 @@ exports.getAreaByStoreId = async (req, res) => {
   } catch (error) {
     console.error("Error fetching area by storeId:", error);
     res.status(500).json({
+
       message:
         "An error occurred while fetching area by storeId. Please try again later.",
     });
